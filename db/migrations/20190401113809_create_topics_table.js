c33830bb95c93fe151fe09d2e585081b3a8426c8
Northcoders13 <northcoders13@Northcoders13s-MacBook-Pro.local>
2019-04-01 12:10:17 +0100

    create_topics_table migration created

--- db/migrations/20190401113809_create_topics_table.js
+++ db/migrations/20190401113809_create_topics_table.js
@@ -1,9 +1,11 @@
 
 exports.up = function(knex, Promise) {
-
-    })
+return knex.schema.createTable('topics', topics => {
+    topics.text('slug').primary;
+    topics.text('descripiton');
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('topics');
 };
