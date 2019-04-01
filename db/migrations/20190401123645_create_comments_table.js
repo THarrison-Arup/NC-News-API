1628afad5191fba020d7b7a8bd0be04aee8bc10d
Northcoders13 <northcoders13@Northcoders13s-MacBook-Pro.local>
2019-04-01 12:42:20 +0100

    create_comments_table migration created

---
+++ db/migrations/20190401123645_create_comments_table.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('comments', comments => {
+    comments.increments('comment_id').primary();
+    comments.text('author').references('users.username');
+    comments.integer('article_id').references('articles.article_id');
+    comments.integer('votes').defaultTo(0);
+    comments.date('created_at').defaultTo(knex.fn.now());
+    comments.text('body');
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('comments');
+};
