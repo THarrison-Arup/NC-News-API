7b6a0c3eae2facb038ec25de5df5f5be20423b4e
Northcoders13 <northcoders13@Northcoders13s-MacBook-Pro.local>
2019-04-02 12:04:54 +0100

    tests added to app.spec.js for topicsRouter

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,24 +1,35 @@
-process.env.NODE_ENV = 'test';
+process.env.NODE_ENV = "test";
 
-const { expect } = require('chai');
-const supertest = require('supertest');
+const { expect } = require("chai");
+const supertest = require("supertest");
 
-const app = require('../app');
-const connection = require('../db/connection');
+const app = require("../app");
+const connection = require("../db/connection");
 
 const request = supertest(app);
 
-describe.only('/', () => {
+describe.only("/", () => {
   beforeEach(() => connection.seed.run());
   after(() => connection.destroy());
 
-  describe('/api', () => {
-    it('GET status:200', () => {
+  describe("/api", () => {
+    it("GET status:200", () => {
       return request
-        .get('/api')
+        .get("/api")
         .expect(200)
         .then(({ body }) => {
           expect(body.ok).to.equal(true);
+        });
+    });
+  });
+
+  describe("/api/topics", () => {
+    it("GET status:200", () => {
+      return request
+        .get("/api/topics")
+        .expect(200)
+        .then(({ body: { msg } }) => {
+          expect(msg).to.equal("OK from the topics router");
         });
     });
   });
