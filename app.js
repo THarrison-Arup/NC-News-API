ec1fedc007e59626dab61dbaf971909e976b58fd
Northcoders13 <northcoders13@Northcoders13s-MacBook-Pro.local>
2019-04-02 14:32:39 +0100

    topics and articles routers moved to api.js route

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const apiRouter = require("./routes/api");
-const topicsRouter = require("./routes/topics");
-const articlesRouter = require("./routes/articles");
 const { routeNotFound, handle500 } = require("./errors");
 
 const app = express();
@@ -9,10 +7,6 @@ const app = express();
 app.use(express.json());
 
 app.use("/api", apiRouter);
-
-app.use("/api/topics", topicsRouter);
-
-app.use("/api/articles", articlesRouter);
 
 app.all("/*", routeNotFound);
 
