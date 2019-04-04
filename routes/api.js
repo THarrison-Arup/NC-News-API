4dcc1f1eca89fed39d0414381bbedca12adac882
Northcoders13 <northcoders13@Northcoders13s-MacBook-Pro.local>
2019-04-04 14:16:20 +0100

    usersRouter added to apiRouter

--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,7 @@ const { methodNotAllowed } = require("../errors");
 const topicsRouter = require("./topics");
 const articlesRouter = require("./articles");
 const commentsRouter = require("./comments");
+const usersRouter = require("./users");
 
 apiRouter
   .route("/")
@@ -15,5 +16,7 @@ apiRouter.use("/topics", topicsRouter);
 apiRouter.use("/articles", articlesRouter);
 
 apiRouter.use("/comments", commentsRouter);
+
+apiRouter.use("/users", usersRouter);
 
 module.exports = apiRouter;
